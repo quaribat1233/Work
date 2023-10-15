2017c17106815f5a0dfc8325e049cb09e41fa6a3
quaribat1233 <quaribatadenle0@gmail.com>
2023-10-15 15:44:16 -0700

    first commit

---
+++ factorial.py
@@ -0,0 +1,17 @@
+#Collecting a positive number
+n = int(input("enter the positive number"))
+# finding the factorial of given number
+factorial = 1
+if n >= 1:
+    for i in range (1,n+1):
+        factorial = factorial * i 
+        print(factorial)
+
+
+
+    
+
+    
+
+
+
